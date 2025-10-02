3b4f6d7cddd8595e2f9ad1682aa4f52f6c891274
Sayed <s.rahman14@hotmail.com>
2025-10-03 00:18:12 +0100

    connect to DB first

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,19 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotdot from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 dotdot.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
 // Middleware
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log("Server started on PORT: ", PORT);
+  });
 });
