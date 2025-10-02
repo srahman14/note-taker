f96bc416022964da2f07ede285330d93bcfdcac6
Sayed <s.rahman14@hotmail.com>
2025-10-02 13:51:53 +0100

    added extra route for finding notes by id

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,11 +1,18 @@
-import express from "express"
-import { createNote, updateNote, deleteNote, getNotes } from "../controllers/notesController.js"
+import express from "express";
+import {
+  createNote,
+  updateNote,
+  deleteNote,
+  getNotes,
+  getNoteById,
+} from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getNotes);
+router.get("/:id", getNoteById);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
 
-export default router
+export default router;
