ad1a3a08efed829ab518cf8382ebac4c4817a116
Sayed <s.rahman14@hotmail.com>
2025-10-08 13:50:20 +0100

    fetching notes and displaying, added loading state and ratelimiter card

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar.jsx";
+import RateLimited from "../components/RateLimited.jsx";
+import axios from "axios";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard.jsx";
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  )
-}
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default HomePage
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        setNotes(res.data);
+        console.log(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        if (error.response && error.response.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to fetch notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="min-h-screen" data-theme="dim">
+      <Navbar />
+      {isRateLimited && <RateLimited />}
+
+      <div className="mx-auto max-w-7xl p-4">
+        {loading && <div className="text-center text-primary">Loading...</div>}
+      </div>
+
+      {notes.length > 0 && !isRateLimited && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {notes.map((note, idx) => (
+            <NoteCard key={note._id || idx} note={note} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
