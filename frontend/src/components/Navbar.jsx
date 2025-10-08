bf0ade3a32f60a78b93ebc096a17167aa81effff
Sayed <s.rahman14@hotmail.com>
2025-10-08 13:50:49 +0100

    navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Plus } from "lucide-react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <header className="bg-base-300 border-b border-b-content/20">
+      <div className="mx-auto max-w-6xl p-4">
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">
+            NoteTaker
+          </h1>
+
+          <div className="flex gap-4 justify-center items-center">
+            <Link to="/create" className="btn btn-primary btn-sm">
+              <Plus className="size-5" />
+              <span>New Note</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
