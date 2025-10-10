56e0520fdee52bbbb85b091cd5d5a9e9919495c3
srahman14 <s.rahman14@hotmail.com>
2025-10-10 22:03:18 +0100

    theme provider wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,19 @@ import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import toast from "react-hot-toast";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 const App = () => {
   return (
-    <div data-theme="dim">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/note/:id" element={<NoteDetailPage />} />
-      </Routes>
-    </div>
+    <ThemeProvider>
+      <div className="transition-colors duration-300">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/note/:id" element={<NoteDetailPage />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 };
 
