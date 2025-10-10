8267b7494383bc4193d2090afabea2e4d418f0ee
srahman14 <s.rahman14@hotmail.com>
2025-10-10 22:05:51 +0100

    theme controller component

---
+++ frontend/src/components/ThemeController.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useTheme } from "../context/ThemeContext";
+
+const ThemeController = () => {
+  const { theme, setTheme } = useTheme();
+
+  const themes = ["dim", "forest", "fantasy", "autumn"];
+
+  return (
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-primary m-1 btn-sm">
+        Theme
+        <svg
+          width="12px"
+          height="12px"
+          className="inline-block h-2 w-2 fill-current opacity-60"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048"
+        >
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
+      >
+        {themes.map((t) => (
+          <li key={t}>
+            <button
+              onClick={() => setTheme(t)}
+              className={`btn btn-sm btn-block btn-ghost justify-start ${
+                theme === t ? "btn-active" : ""
+              }`}
+            >
+              {t.charAt(0).toUpperCase() + t.slice(1)}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ThemeController;
