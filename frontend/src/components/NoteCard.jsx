96c07656919e0bf523b2875978130e7c3276923b
srahman14 <s.rahman14@hotmail.com>
2025-10-10 22:04:22 +0100

    updated UI stylings

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -5,9 +5,10 @@ import api from "../lib/axios";
 import toast from "react-hot-toast";
 
 const NoteCard = ({ note, setNotes }) => {
+
   const handleDelete = async (e, id) => {
     e.preventDefault();
-
+    
     if (!window.confirm("Are you sure you want to delete this note?")) {
       return;
     }
@@ -19,24 +20,21 @@ const NoteCard = ({ note, setNotes }) => {
       window.location.reload();
     } catch (error) {
       console.error("Error deleting note", error);
-      toast.error("Failed to delete note");
+      toast.error("Failed to delete note");      
     }
+
   };
 
-  return (
+  return ( 
     <Link
       to={`/note/${note._id}`}
-      className="card bg-neutral hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-primary p-4 rounded-lg"
+      className="card bg-primary-content hover:shadow-lg transition-all duration-200 border-t-8 border-solid border-primary p-4 rounded-lg"
     >
       <div className="card-body">
-        <h3 className="card-title text-base-content font-semibold mb-2">
-          {note.title}
-        </h3>
-        <p className="text-base-content/70 line-clamp-3 text-sm text-gray-600">
-          {note.content}
-        </p>
+        <h3 className="card-title text-primary font-semibold mb-2">{note.title}</h3>
+        <p className="line-clamp-3 text-sm font-bold text-primary">{note.content}</p>
         <div className="card-actions justify-between items-center mt-4">
-          <span className="text-sm text-base-content/50">
+          <span className="text-sm text-base-content-300">
             {new Date(note.createdAt).toLocaleDateString("en-US", {
               year: "numeric",
               month: "short",
@@ -45,12 +43,9 @@ const NoteCard = ({ note, setNotes }) => {
           </span>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4 text-base-content/70" />
-            <button
-              className="btn btn-ghost btn-xs"
-              onClick={(e) => {
-                handleDelete(e, note._id);
-              }}
-            >
+            <button className="btn btn-ghost btn-xs" onClick={(e) => {
+              handleDelete(e, note._id);
+            }}>
               <Trash2Icon className="size-4 text-base-content/70 text-error" />
             </button>
           </div>
