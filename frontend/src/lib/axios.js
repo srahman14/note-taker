3801303acb0cc584c27a50cde0ea0008f743c70f
srahman14 <s.rahman14@hotmail.com>
2025-10-10 23:54:08 +0100

    udpated for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-import path from "path";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
-const api = axios.create({
-  baseURL: BASE_URL,
-});
-
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+const api = axios.create({ baseURL: BASE_URL });
 export default api;
