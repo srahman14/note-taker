d61ee44a7b44908548efe79af7657a2bd69d903d
srahman14 <s.rahman14@hotmail.com>
2025-10-11 00:33:40 +0100

    deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001/api"
-  : "/api";
+const BASE_URL = import.meta.env.PROD
+  ? "/api"
+  : "http://localhost:5001/api";
 
 const api = axios.create({ baseURL: BASE_URL });
 
