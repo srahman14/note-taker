2ac06658af9b2bca2b4a314cdb097ab226137188
Sayed <s.rahman14@hotmail.com>
2025-10-11 19:56:22 +0100

    added UserID in schema

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -3,18 +3,25 @@ import mongoose from "mongoose";
 // SCHEME
 // MODEL based off the scheme
 
-const noteScheme = new mongoose.Schema({
+const noteScheme = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     content: {
-        type: String,
-        required: true,
-    }
-}, {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
 const Note = mongoose.model("Note", noteScheme);
 
