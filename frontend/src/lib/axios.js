8ec18a155047ad292454cbbb2137fe0fc0989d99
Sayed <s.rahman14@hotmail.com>
2025-10-11 19:59:12 +0100

    added firebase token

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,22 @@
 import axios from "axios";
+import { getAuth } from "firebase/auth";
 
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
+});
+
+// Attach Firebase token automatically
+api.interceptors.request.use(async (config) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user) {
+    const token = await user.getIdToken();
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
