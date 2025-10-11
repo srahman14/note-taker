bf9fbfe44c10d0d83587bd04e2bdb7f89222e7a1
Sayed <s.rahman14@hotmail.com>
2025-10-11 16:52:14 +0100

    base url change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.PROD
-  ? "/api"
-  : "http://localhost:5001/api";
-
-const api = axios.create({ baseURL: BASE_URL });
+const api = axios.create({ baseURL: "/api" });
 
 export default api;
