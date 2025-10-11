525339c51cf8bb4c6f0bdaa7007a3fccafedaae7
Sayed <s.rahman14@hotmail.com>
2025-10-11 19:58:38 +0100

    added auth provider wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,25 @@ import CreatePage from "./pages/CreatePage.jsx";
 import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
+import SignUpOrLogin from "./pages/SignUpOrLogin.jsx";
 import toast from "react-hot-toast";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <div className="transition-colors duration-300">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreatePage />} />
-          <Route path="/note/:id" element={<NoteDetailPage />} />
-        </Routes>
-      </div>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <div className="transition-colors duration-300">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/note/:id" element={<NoteDetailPage />} />
+            <Route path="/auth" element={<SignUpOrLogin />} />
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
 
