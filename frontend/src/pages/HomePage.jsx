00fa3a860c87a7dcd23a448e280d2f809f289cae
Sayed <s.rahman14@hotmail.com>
2025-10-11 19:59:43 +0100

    added user auth

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,18 +5,30 @@ import api from "../lib/axios.js";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard.jsx";
 import NotesNotFound from "../components/NotesNotFound.jsx";
+import { useAuth } from "../context/AuthContext.jsx";
+import { useNavigate } from "react-router";
 
 const HomePage = () => {
+  const { user, loading: authLoading } = useAuth();
+  const navigate = useNavigate();
+
   const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (!authLoading && !user) {
+      // redirect to login if not logged in
+      navigate("/auth");
+      return;
+    }
+
     const fetchNotes = async () => {
+      if (!user) return;
+
       try {
-        const res = await api.get("/notes");
+        const res = await api.get("/notes"); // backend already filters by userId
         setNotes(res.data);
-        console.log(res.data);
         setIsRateLimited(false);
       } catch (error) {
         if (error.response && error.response.status === 429) {
@@ -28,8 +40,9 @@ const HomePage = () => {
         setLoading(false);
       }
     };
+
     fetchNotes();
-  }, []);
+  }, [user, authLoading, navigate]);
 
   return (
     <div className="min-h-screen">
