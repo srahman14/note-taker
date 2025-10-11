9edb6232aa9df229111b9d74c704bcf885d15ae9
Sayed <s.rahman14@hotmail.com>
2025-10-11 19:58:51 +0100

    added logout button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { Plus } from "lucide-react";
+import React, { useEffect } from "react";
+import { LogOutIcon, Plus } from "lucide-react";
 import { Link } from "react-router";
 import ThemeController from "./ThemeController";
+import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logout } = useAuth();
+
   return (
     <header className="bg-base-300 border-b border-b-content/20">
       <div className="mx-auto max-w-6xl p-4">
@@ -22,6 +25,23 @@ const Navbar = () => {
 
             <div>
               <ThemeController />
+            </div>
+
+            <div>
+              {user ? (
+                <button
+                  onClick={logout}
+                  className="btn btn-primary btn-sm flex items-center gap-2"
+                >
+                  <LogOutIcon className=" h-5 w-5" />{" "}
+                  <span className="hidden md:inline">Logout</span>
+                  {/* replace with an icon you like */}
+                </button>
+              ) : (
+                <Link to="/auth" className="btn btn-primary btn-sm md:btn-md">
+                  Login / Signup
+                </Link>
+              )}
             </div>
           </div>
         </div>
